3870f271dd8a41ee61bc7bedef85b6ee49461a85
Kenenth <kennethhj04@gmail.com>
2023-09-22 00:08:25 -0400

    backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import MongoClient from 'mongodb';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import express from 'express';
+
+const app = express()
+
+const dbURI = 'mongodb+srv://test1:test123@data.23manba.mongodb.net/?retryWrites=true&w=majority'
+
+console.log("Starting...");
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('worked')
+    app.listen(3000)
+  })
+  .catch(err => console.log(err));
+
+  console.log('hello wolrd')
