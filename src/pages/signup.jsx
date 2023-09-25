7741dd28fc994bf02e7b18f702e19b5e37ea5986
Kenenth <kennethhj04@gmail.com>
2023-09-25 00:18:11 -0400

    added style to signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../components/Api';
+import CloseIcon from '@mui/icons-material/Close';
+import '../cssfolder/signup.css'
 
 function SignUp() {
   const navigate = useNavigate();
@@ -39,13 +41,30 @@ function SignUp() {
 
   return (
     <div className='signup-container'>
-        <h1>Signup</h1>
-        <form onSubmit={submit}>
-            <input type="text" onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" />
-            <input type="text" onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" />
-            <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-            <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-            <input type="submit" value="Sign Up" />
+      <div className='signup-navbar'>
+        <div className='signup-header'>
+          <h2>Sign Up</h2>        
+        </div>
+      </div>
+        <form onSubmit={submit} className='navbar-form'>
+          <div className='name-section'>
+            <input type="text" onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" className='input-fields first'/>
+            <input type="text" onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" className='input-fields last'/>
+          </div>
+            <p className='signup-paragraph'>Make sure it matches the name on your government ID.</p>
+            <input type="birthdate" onChange={(e) => setEmail(e.target.value)} placeholder="Birthdate (mm/dd/yyyy)" className='input-fields birthdate' />
+            <p className='signup-paragraph'>To sign up, you need to be at least 18. Your birthday won't be shared with other people who use Airbnb.</p>
+            <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email" className='input-fields birthdate' />
+            <p className='signup-paragraph'>We'll email you trip confirmations and reciepts.</p>
+            <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password" className='input-fields birthdate'/>
+            <p className='signup-paragraph'>We'll send you marketing promotions, special offers, inspiration and policy updates via email.</p>
+            
+            <div className='checkbox-section'>
+              <input type='checkbox' className='checkbox'/>
+              <p>I don't want to recieve marketing messages from Airbnb. I can also opt out of recieving these at anytime in my account settings or via the link in the message.</p>
+            </div>
+            <p>By selecting <string>Agree and continue</string> below, I agree to Airbnb's Terms of Service, Payments Terms of Service, Privacy Policy, and Nondiscrimination Policy</p>
+            <input type="submit" value="Sign Up" className='airbnb-button'/>
         </form>
     </div>
   );
